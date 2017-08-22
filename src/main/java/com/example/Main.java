6675d22aa2b739af84aed003b53f51037bae95f9
DESKTOP-EBC4AID\manish <manish.kakkar@hva.nl>
2017-08-22 14:29:41 +0200

    first-commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@SpringBootApplication
+public class Main {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(100000);
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
+    @RequestMapping("/db")
+    String db(Map<String, Object> model) {
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+            ArrayList<String> output = new ArrayList<String>();
+            while (rs.next()) {
+                output.add("Read from DB: " + rs.getTimestamp("tick"));
+            }
+
+            model.put("records", output);
+            return "db";
+        } catch (Exception e) {
+            model.put("message", e.getMessage());
+            return "error";
+        }
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
+
+    //  @RequestMapping("/latestsensordata", method = POST)
+//  @ResponseBody
+    @PostMapping("/latestsensordata")
+    @ResponseBody
+    public String getFoosBySimplePath(@RequestParam("lat") Double latitude,
+                                      @RequestParam("long") Double longitude) {
+
+        System.err.println("request started");
+
+        List<SensorValue> results = new ArrayList<SensorValue>();
+        SensorValue filteredSensorValue = null;
+
+        try {
+            String allData = getHTML("http://api.luftdaten.info/static/v1/data.json");
+            System.err.println("this is not reached");
+
+            JSONArray json = new JSONArray(allData);
+
+            for (int i = 0; i < json.length(); i++) {
+                SensorValue value = new SensorValue();
+                JSONObject object = (JSONObject) json.get(i);
+                JSONObject locationObject = object.getJSONObject("location");
+                value.setLat(locationObject.getDouble("latitude"));
+                value.setLng(locationObject.getDouble("longitude"));
+
+                JSONArray sensorValues = object.getJSONArray("sensordatavalues");
+                System.err.println("here");
+
+                boolean isPSensor = false;
+                for (int s = 0; s < sensorValues.length(); s++) {
+                    JSONObject sensorValueObject = (JSONObject) sensorValues.get(s);
+                    String valueType = sensorValueObject.getString("value_type");
+
+                    if (valueType.equals("P1")) {
+                        value.setP1(sensorValueObject.getDouble("value"));
+                        isPSensor = true;
+                    }
+                    if (valueType.equals("P2")) {
+                        value.setP2(sensorValueObject.getDouble("value"));
+                        isPSensor = true;
+                    }
+                }
+
+                System.err.println("TEST 2");
+
+                value.setTimestamp(object.getString("timestamp"));
+
+                results.add(value);
+            }
+
+        } catch (Exception e) {
+            System.err.println("exception " + e.toString());
+            e.printStackTrace();
+        }
+
+        filteredSensorValue = filterByLocationAndTime(results, new Location("requestLocation", latitude, longitude));
+
+        if (filteredSensorValue != null) {
+            return filteredSensorValue.getP1() + "";
+        } else {
+            return "sensorvalue not found";
+        }
+    }
+
+    public SensorValue filterByLocationAndTime(List<SensorValue> originalList, Location myLocation) {
+        List<SensorValue> filteredList = new ArrayList<SensorValue>();
+        Location closestLocation = searchClosestLocation(originalList, myLocation);
+
+        System.err.println("TEST 3");
+
+        for (SensorValue value : originalList) {
+            if (value.getLocation().equals(closestLocation)) {
+                filteredList.add(value);
+            }
+        }
+
+        return filterByTime(filteredList);
+
+    }
+
+    public Location searchClosestLocation(List<SensorValue> originalList, Location myLocation) {
+        float closestDistance = 999F;
+        Location closestLocation = null;
+        for (SensorValue value : originalList) {
+            if (value.getLocation().distanceTo(myLocation) < closestDistance) {
+                closestLocation = new Location(value.getLocation());
+            }
+
+        }
+        return closestLocation;
+
+    }
+
+    public SensorValue filterByTime(List<SensorValue> originalList) {
+        Date closestDate = originalList.get(0).getTimestamp();
+        SensorValue filteredSensorValue = new SensorValue();
+
+        for (SensorValue value : originalList) {
+            if (closestDate.after(value.getTimestamp())) {
+                filteredSensorValue = value;
+            }
+        }
+
+        return filteredSensorValue;
+    }
+
+}
---
+++ src/main/java/com/example/SensorValue.java
@@ -0,0 +1,78 @@
+package com.example;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SensorValue {
+    private Date timestamp;
+    private double lat;
+    private double lng;
+    private int sensorId;
+    private double p1;
+    private double p2;
+
+    public SensorValue() {
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            this.timestamp = formatter.parse(timestamp);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public int getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(int sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public double getP1() {
+        return p1;
+    }
+
+    public void setP1(double p1) {
+        this.p1 = p1;
+    }
+
+    public double getP2() {
+        return p2;
+    }
+
+    public void setP2(double p2) {
+        this.p2 = p2;
+    }
+
+    public Location getLocation() {
+        Location location = new Location("Point", this.getLat(), this.getLng());
+        return location;
+    }
+
+
+}
